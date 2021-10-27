853bd3aefe07d316b97732ea24775e6a5ca0a3f2
Hadi Pd <h7ad3i@chmail.ir>
2021-10-27 07:09:40 +0330

    contact mail controller & test

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CodePortal</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    <p>{{ $details['body'] }}</p>
+   
+    <p>-- Team CodePortal</p>
+</body>
+</html>
