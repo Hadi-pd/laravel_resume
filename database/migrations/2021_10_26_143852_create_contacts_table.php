e8ba750c500245fcb49566927d7ba7cb240ba673
Hadi Pd <h7ad3i@chmail.ir>
2021-10-27 07:07:20 +0330

    contact model & migration

---
+++ database/migrations/2021_10_26_143852_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->string('message',1000);
+            $table->boolean('is_sent')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
